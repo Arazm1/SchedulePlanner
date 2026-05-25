77b865ccfcb90a5e39c4d563fa27cedcafc31d22
Arazm1 <araz.muhamm3d@gmail.com>
2026-05-25 22:55:47 +0300

    Created ShiftRequest DTO for incoming shift creation requests.

---
+++ backend/src/main/java/app/dto/ShiftRequest.java
@@ -0,0 +1,60 @@
+package app.dto;
+
+import java.time.LocalDateTime;
+
+public class ShiftRequest {
+    private String shiftName;
+    private String shiftInfo;
+    private String shiftNotes;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Integer companyID;
+
+    public String getShiftName() {
+        return this.shiftName;
+    }
+
+    public void setShiftName(String shiftName) {
+        this.shiftName = shiftName;
+    }
+
+    public String getShiftInfo() {
+        return this.shiftInfo;
+    }
+
+    public void setShiftInfo(String shiftInfo) {
+        this.shiftInfo = shiftInfo;
+    }
+
+    public String getShiftNotes() {
+        return this.shiftNotes;
+    }
+
+    public void setShiftNotes(String shiftNotes) {
+        this.shiftNotes = shiftNotes;
+    }
+
+    public LocalDateTime getStartTime() {
+        return this.startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return this.endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Integer getCompanyID() {
+        return this.companyID;
+    }
+
+    public void setCompanyID(Integer companyID) {
+        this.companyID = companyID;
+    }
+}
